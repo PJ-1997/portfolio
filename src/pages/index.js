007229c5d4f0152df97412ae091ccf6731fcd184
Pedro Molina <jrmolina180@gmail.com>
2019-11-02 14:56:10 -0700

    Added Image

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,17 @@ import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <Layout>
-    <div className="w-container">
-      <h1 className="header">Pedro Molina</h1>
-      <div className="description">iOS Developer and Graphic Designer with years of experience in visual and interaction design.
+  <body className="body">
+    {/* Introduction */}
+    <div className="introduction" >
+      <div className="w-container">
+        <h1 className="header">Pedro Molina</h1>
+        <div className="description">iOS Developer and Graphic Designer with years of experience in visual and interaction design.
     </div>
+      </div>
     </div>
+    {/* Spacer */}
+    <div className="layout-spacer-top"></div>
 
     {/* App Projects */}
     <div id="Projects" className="projects">
@@ -22,15 +27,31 @@ const IndexPage = () => (
         <div className="projectinfo">
           <div className="projecttitle">Quick Formula</div>
           <div className="projectdates">Aug 2017 - Present</div>
-          <div className="projectdescription">Quick Formula calculates math and science formulas. Uses a segmented control to separate math and science formulas. Available in the App Store. Quick Formula is built entirely in Swift and the UI is created programmatically with no interface builder.</div>
+          <div className="projectdescription">Quick Formula calculates math and science formulas. Uses a segmented control to separate math and science formulas. Quick Formula is built entirely in Swift and the UI is created programmatically with no interface builder.</div>
           <div className="link projects">VIEW CASE STUDY</div>
+        </div>
+      </div>
+    </div>
+
+    <div id="Projects" className="projects">
+      <div className="project w-container">
+        <div className="projectimagebg">
+          <a href="" className="projectimage w-inline-block"></a>
+        </div>
+        <div className="projectinfo">
+          <div className="projecttitle">Stock Notify</div>
+          <div className="projectdates">Aug 2017 - Present</div>
+          <div className="projectdescription">Sends a push notification when a stock reaches a price that the user specified. The user can favorite stocks which uses Core Data to persist the data. The user interface is built all programmatically.</div>
+          <a className="link projects" href="/works"> View Case</a>
         </div>
       </div>
     </div>
 
     {/* <SEO title="Home" /> */}
     {/* <Link to="/page-2/">Go to page 2</Link> */}
-  </Layout>
+  </body>
+
+
 )
 
 export default IndexPage
