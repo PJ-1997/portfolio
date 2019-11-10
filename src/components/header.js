cbcfc46c46454df11a609f906d41b0a81e52319e
Pedro Molina <jrmolina180@gmail.com>
2019-11-09 21:03:15 -0800

    Name Changes

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ import React from "react"
 
 const Header = ({ siteTitle }) => (
   <header style={{background: "black",marginBottom: `1.45rem`,}}>
-    <div style={{margin: `0 auto`, maxWidth: 960, padding: `1.45rem 1.0875rem`,}}>
+    <div style={{margin: `0 auto`, maxWidth: 960, padding: `1rem 1.0875rem`,}}>
       <h1 style={{ margin: 0 }}>
         <Link to="/" style={{ color: `white`, textDecoration: `none`,}}>
           {siteTitle}
