40ecd64d81b15d4b4e6adf92f1018925a2ee0d1a
Pedro Molina <jrmolina180@gmail.com>
2020-06-26 22:42:42 -0700

    Module CSS

--- src/pages/stocknotifycase.js
+++ src/pages/stocknotifycase.js
@@ -3,25 +3,34 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 import Rotato from "../images/Rotato.png"
+import containerStyles from "../components/Modules/casestudy.module.css"
 
 const CaseStudy = () => (
   <Layout>
     <body className="body">
 
-      <section className="introsection">
-        <div className="divcenter">
-
-          <div className="lefttext-container">
-            <h1 className="toptext">Stock Notify</h1>
-            <div className="bottomtext">This is the codfdradf dfljdfl dlfkdjfjdfdfdfdf<br></br>dfasdfsdf lsdkjfsdjf</div>
+      <section className={containerStyles.introsection}>
+        <div className={containerStyles.divcenter}>
+          <div className={containerStyles.lefttextcontainer}>
+            <h1 className={containerStyles.toptext}>Stock Notify</h1>
+            <div className={containerStyles.bottomtext}>This is the codfdradf dfljdfl dlfkdjfjdfdfdfdf<br></br>dfasdfsdf lsdkjfsdjf</div>
           </div>
-
-          <div className="IphonePic">
-            <img src={Rotato} className="ratioimage" ></img>
+          <div className={containerStyles.IphonePic}>
+            <img src={Rotato} className={containerStyles.ratioimage}></img>
           </div>
-
         </div>
       </section>
+
+      <div className={containerStyles.layoutspacertop}></div>
+
+      <section className={containerStyles.sectionaligncenter}>
+        <div className={containerStyles.titlecontainer}>
+          <h1 className={containerStyles.titletext}>Overview</h1>
+          <div className={containerStyles.underlinetitle}></div>
+        </div>
+        
+      </section>
+
     </body>
 
     <SEO title="Case Study" />
