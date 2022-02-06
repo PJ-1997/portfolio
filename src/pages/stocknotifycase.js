7f0f8ac31724380bff978250f746bc0deb7b83ae
Pedro Molina <jrmolina180@gmail.com>
2022-02-05 17:58:24 -0800

    Popup crap stocknotify tookout

--- src/pages/stocknotifycase.js
+++ src/pages/stocknotifycase.js
@@ -3,7 +3,6 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import caseStyle from "../components/Modules/casestudy.module.css"
 import { Link } from "gatsby"
-import ImagePopup from "../components/Popups/popup"
 
 import rotatoImg from "../images/stocknotify-imgs/rotato.png"
 
@@ -122,8 +121,7 @@ const CaseStudy = () => (
             <div className={caseStyle.flexTextContainer}>
               <p className={caseStyle.titleParagraph}>Lo-Fidelity Prototyping</p>
               <p className={caseStyle.subjectText}>I like to get my ideas on paper before I move into digitally creating the lo-fidelity prototypes.<br></br><br></br>Once I’ve gone through a couple of iterations, I then shared these lo-fidelity prototypes with users to get some feedback. This is an important step to make sure that the design fulfills the users’ need.</p>
-              <ImagePopup title="Lo-Fidelity Wireframes" popimage={lofiPopup}
-                triggerElement={<img className={caseStyle.styleguideImg} src={lofiMain} alt="styleimg"></img>}></ImagePopup>
+              <img src={lofiMain} alt="styleimg" className={caseStyle.styleguideImg}></img>
               <p className={caseStyle.imgdescriptext}>Lo-Fidelity Wireframes</p>
             </div>
           </div>
@@ -132,8 +130,7 @@ const CaseStudy = () => (
             <div className={caseStyle.flexTextContainer}>
               <p className={caseStyle.titleParagraph}>Evaluating Initial Wireframes</p>
               <p className={caseStyle.subjectText}>The first iteration of the set up notification screen was originally going to incorporate a slide menu showing the user different options which included setting the price notification either higher, lower, or equal than the price that the user typed in.<br></br><br></br> This iteration involved too much interaction from the user. From the user feedback, I aimed to create the notify screen more simple to the user.</p>
-              <ImagePopup title="Initial Iteration of Notify Screen" popimage={initialPopup}
-                triggerElement={<img className={caseStyle.styleguideImg} src={initialScreen} alt="styleimg"></img>}></ImagePopup>
+              <img src={initialScreen} alt="styleimg" className={caseStyle.styleguideImg}></img>
               <p className={caseStyle.imgdescriptext}>Initial Iteration of Notify Screen</p>
             </div>
           </div>
@@ -142,13 +139,11 @@ const CaseStudy = () => (
             <div className={caseStyle.flexTextContainer}>
               <p className={caseStyle.titleParagraph}>Hi-Fidelity Wireframes</p>
               <p className={caseStyle.subjectText}>I chose to only give the user two options. Either set the price notification	 higher or lower than the inputed price. And I also removed the slide menu so that the options are apparent to the user and not hidden away through a slide menu. This new iteration is simple and improved the task of setting a price notification.</p>
-              <ImagePopup title="Comparing Iterations" popimage={comparePopup}
-                triggerElement={<img className={caseStyle.styleguideImg} src={compare} alt="styleimg"></img>}></ImagePopup>
+              <img src={compare} alt="styleimg" className={caseStyle.styleguideImg}></img>
               {/* <img className={caseStyle.styleguideImg} src={compare} alt="styleimg"></img> */}
               <p className={caseStyle.imgdescriptext}>Comparing Iterations</p>
               <p className={caseStyle.bottomImageST}>I also chose to color code the type of notification. For example, green is above and red is below. I kept this concept when creating the home screen for the application. The user is able to quickly see what notifications are still active and tell how the notified stock will activate based on the color.</p>
-              <ImagePopup title="Hi-Fidelity Wireframes" popimage={hifiPopup}
-                triggerElement={<img className={caseStyle.styleguideImg} src={hifiMain} alt="styleimg"></img>}></ImagePopup>
+              <img src={hifiMain} alt="styleimg" className={caseStyle.styleguideImg}></img>
 
               <p className={caseStyle.imgdescriptext}>Hi-Fidelity Wireframes</p>
             </div>
